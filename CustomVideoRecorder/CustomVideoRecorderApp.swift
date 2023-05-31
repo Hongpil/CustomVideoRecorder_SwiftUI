ea7e3ee7d56ddccaeb943a65284e93c8fe22f380
kimhongpil <idpil@kimhongpils-MacBook-Pro.local>
2023-05-31 10:02:00 +0900

    init

---
+++ CustomVideoRecorder/CustomVideoRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomVideoRecorderApp.swift
+//  CustomVideoRecorder
+//
+//  Created by kimhongpil on 2023/05/31.
+//
+
+import SwiftUI
+
+@main
+struct CustomVideoRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
